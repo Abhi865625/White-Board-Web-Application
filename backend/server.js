f7915591ee718d4e1fd0dfc8eee5248b2b78c31a
Abhishek Kumar Mehta <akmehta7252@gmail.com>
2023-12-24 00:14:07 +0530

    comit

---
+++ backend/server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+const { Server } = require("socket.io");
+
+const { addUser, getUser, removeUser } = require("./utils/users");
+
+const { PeerServer } = require("peer");
+
+const peerServer = PeerServer({
+  port: 5001,
+  path: "/",
+});
+
+const io = new Server(server);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.send(
+    "This is mern realtime board sharing app official server by Abhishek Kumar Mehta"
+  );
+});
+
+let roomIdGlobal, imgURLGlobal;
+
+io.on("connection", (socket) => {
+  socket.on("userJoined", (data) => {
+    const { name, userId, roomId, host, presenter } = data;
+    roomIdGlobal = roomId;
+    socket.join(roomId);
+    const users = addUser({
+      name,
+      userId,
+      roomId,
+      host,
+      presenter,
+      socketId: socket.id,
+    });
+    socket.emit("userIsJoined", { success: true, users });
+    console.log({ name, userId });
+    socket.broadcast.to(roomId).emit("allUsers", users);
+    setTimeout(() => {
+      socket.broadcast
+        .to(roomId)
+        .emit("userJoinedMessageBroadcasted", { name, userId, users });
+      socket.broadcast.to(roomId).emit("whiteBoardDataResponse", {
+        imgURL: imgURLGlobal,
+      });
+    }, 1000);
+  });
+
+  socket.on("whiteboardData", (data) => {
+    imgURLGlobal = data;
+    socket.broadcast.to(roomIdGlobal).emit("whiteBoardDataResponse", {
+      imgURL: data,
+    });
+  });
+
+  socket.on("message", (data) => {
+    const { message } = data;
+    const user = getUser(socket.id);
+    if (user) {
+      socket.broadcast
+        .to(roomIdGlobal)
+        .emit("messageResponse", { message, name: user.name });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const user = getUser(socket.id);
+    if (user) {
+      removeUser(socket.id);
+      socket.broadcast.to(roomIdGlobal).emit("userLeftMessageBroadcasted", {
+        name: user.name,
+        userId: user.userId,
+      });
+    }
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () =>
+  console.log("server is running on http://localhost:5000")
+);
