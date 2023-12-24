c0f44b218bda2bfc3d7f1d160daf74f8637aca39
Abhishek Kumar Mehta <93569162+Abhi865625@users.noreply.github.com>
2023-12-24 09:25:26 +0530

    Add files via upload

---
+++ frontend/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "react-toastify/dist/ReactToastify.min.css";
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>,
+)
